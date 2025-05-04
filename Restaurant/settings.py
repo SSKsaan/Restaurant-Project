485e423e4ca94178d0eec86da8166ff1ef64e611
SaminSadik <Saminsadikkhan@gmail.com>
2025-05-04 10:22:54 +0600

    added decouple for .env

--- Restaurant/settings.py
+++ Restaurant/settings.py
@@ -19,8 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+from decouple import config
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qgij0q@&xnjioid42@5t-qg_ee18lrbe@ljcd-*r%vs8w@-hsh'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
