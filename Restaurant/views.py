75fc10b65df6944b3587c71fc22898e9f60fe2c5
SaminSadik <Saminsadikkhan@gmail.com>
2025-05-04 22:26:29 +0600

    added correct TemplateView

--- Restaurant/views.py
+++ Restaurant/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth.views import TemplateView
+from django.views.generic import TemplateView
 
 # Create your views here.
 class HomeView(TemplateView):
