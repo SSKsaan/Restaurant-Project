b9b96028312b0c32b96f3a66248b8d8d6d55b125
SaminSadik <Saminsadikkhan@gmail.com>
2025-05-05 12:39:25 +0600

    Added Menu Models with Admin setup

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'category_slug': ('category_name',)}
+admin.site.register(models.Category, CategoryAdmin)
+
+class MenuItemAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'menu_slug': ('item_name',)}
+    list_display = ['item_name', 'price', 'category', 'is_available', 'is_featured']
+    list_editable = ['is_available', 'is_featured']
+    search_fields = ['item_name']
+admin.site.register(models.MenuItem, MenuItemAdmin)
--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, primary_key=True)
+    category_slug = models.SlugField(blank=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+    def __str__ (self):
+        return self.category_name
+    
+class MenuItem(models.Model):
+    item_name = models.CharField(max_length=50, primary_key=True)
+    description = models.TextField(blank=True)
+    item_image = models.ImageField(upload_to="menu")
+    price = models.FloatField()
+    is_available = models.BooleanField(default=True)
+    is_featured = models.BooleanField(default=False)
+    menu_slug = models.SlugField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_DEFAULT, default="Uncategorized")
