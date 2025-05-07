e1c368cc69851b69d7e7c17a033072d4f81d6936
SaminSadik <Saminsadikkhan@gmail.com>
2025-05-07 21:44:42 +0600

    Updated Admin

--- menu/admin.py
+++ menu/admin.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
+@admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'category_slug': ('category_name',)}
-admin.site.register(models.Category, CategoryAdmin)
 
+@admin.register(models.MenuItem)
 class MenuItemAdmin(admin.ModelAdmin):
     prepopulated_fields = {'item_slug': ('item_name',)}
     list_display = ['item_name', 'price', 'category', 'is_available', 'is_featured']
     list_editable = ['is_available', 'is_featured']
     search_fields = ['item_name']
-admin.site.register(models.MenuItem, MenuItemAdmin)
