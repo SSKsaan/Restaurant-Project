a13624ef1baeae677fa8430d37359a4ae5e757bc
SaminSadik <Saminsadikkhan@gmail.com>
2025-05-10 15:39:40 +0600

    simplified js for my use case

--- static/js/cart.js
+++ static/js/cart.js
@@ -2,17 +2,9 @@ function addToCart(itemSlug) {
   fetch(`/menu/add_to_cart/${itemSlug}/`, {
     method: "POST",
     headers: {
-      "X-CSRFToken": getCSRF(),
-      "Content-Type": "application/x-www-form-urlencoded"
+      "X-CSRFToken": document.cookie.split('; ').find(row => row.startsWith('csrftoken='))?.split('=')[1]
     }
   })
   .then(res => res.json())
   .then(data => { if(data.message) {showAlert(data.message);} });
-}
-
-function getCSRF() {
-    return document.cookie
-        .split('; ')
-        .find(row => row.startsWith('csrftoken='))
-        ?.split('=')[1];
 }
