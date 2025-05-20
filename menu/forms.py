32d75d26c7fd6bbdf2c613986edca06de43ffbe5
SaminSadik <Saminsadikkhan@gmail.com>
2025-05-21 01:57:12 +0600

    Added widgets to Content field

--- menu/forms.py
+++ menu/forms.py
@@ -3,7 +3,14 @@ from .models import Review
 
 class ReviewForm(forms.ModelForm):
     image = forms.ImageField(required=False)
-    content = forms.CharField(required=True)
+    content = forms.CharField(
+        required=True,
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'rows': 4,
+            'placeholder': 'Write your review here...'
+        })
+    )
     rating = forms.IntegerField(required=True, min_value=1, max_value=5)
     class Meta:
         model = Review
