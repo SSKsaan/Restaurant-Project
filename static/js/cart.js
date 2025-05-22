f83345b9217b7181087ea1c1c00007d21a52b515
SaminSadik <Saminsadikkhan@gmail.com>
2025-05-22 18:00:04 +0600

    corrected fetching mistake

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,5 +1,5 @@
 function addToCart(itemSlug) {
-  fetch(`/menu/add_to_cart/${itemSlug}/`, {
+  fetch(`/menu/add_to_cart/${itemSlug}`, {
     method: "POST",
     headers: {
       "X-CSRFToken": document.cookie.split('; ').find(row => row.startsWith('csrftoken='))?.split('=')[1]
