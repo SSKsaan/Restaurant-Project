c9c82d0555ac9f98c0266e41eda30ac25141bbb0
SaminSadik <Saminsadikkhan@gmail.com>
2025-05-25 22:00:17 +0600

    synced js and django messages in the template

--- static/js/alert.js
+++ static/js/alert.js
@@ -1,11 +1,11 @@
 function showAlert(message) {
   const alert = document.createElement("div");
-  alert.className = "alert alert-success alert-dismissible m-3";
-  alert.style = "position: fixed; bottom: 10px; right: 10px; z-index: 1000;";
+  alert.className = "alert alert-info alert-dismissible";
   alert.innerHTML = `
     <div>${message}</div>
     <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
   `;
-  document.body.appendChild(alert);
+  const container = document.getElementById("Alert");
+  if (container) container.appendChild(alert);
   setTimeout(() => alert.remove(), 3000);
 }
